2013dd188dfb4832e6ac9b2003f967b2ebf884f2
obviouslyrealname <mircea.hothazie@student.tuiasi.ro>
2026-05-14 11:32:45 +0300

    feat: Lab 12 Tema 1 & Tema 2 a)

---
+++ README.md
@@ -0,0 +1,4 @@
+# Installation
+```bash
+  ./install_deps.sh
+```
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const Database = require("better-sqlite3");
 
 const app = express();
 
@@ -12,12 +13,42 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 
+const cumparaturiDb = new Database("cumparaturi.db")
+
+function getProduse() {
+  return cumparaturiDb.prepare("SELECT * FROM produse").all();
+}
+
 app.get('/', (req, res) => {
-  res.render('index');
+  const produse = getProduse();
+  res.render('index',  { produse }  );
 });
 
 app.get('/autentificare', (req, res) => {
   res.render('autentificare');
+});
+
+app.get('/create-db', (req, res) => {
+  console.log("Create db");
+  cumparaturiDb.prepare(`
+    CREATE TABLE IF NOT EXISTS produse (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      price REAL NOT NULL
+    );
+    `).run();
+  const produse = getProduse();
+  res.render('index',  {produse} );
+});
+
+app.get('/insert-db', (req, res) => {
+  console.log("Insert db");
+  cumparaturiDb.prepare(`
+    INSERT INTO produse(name, price) VALUES(?, ?);
+  `).run("Honda Civic", 67);
+  
+  const produse = getProduse();
+  res.render('index',  { produse } );
 });
 
 app.listen(port, () => {
