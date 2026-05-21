cb1b7b414b1fec05a122d1802bd0e5173b7b8f01
obviouslyrealname <mircea.hothazie@student.tuiasi.ro>
2026-05-21 10:48:43 +0300

    feat: Lab13_Tema1: SQL injection defense mechanism and Users table

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcypt');
 const Database = require("better-sqlite3");
 
 const app = express();
@@ -18,7 +19,38 @@ function getProduse() {
   return cumparaturiDb.prepare("SELECT * FROM produse").all();
 }
 
+class User {
+  public constructor(username, hashedPassword) {
+    this.username = username;
+    this.hashedPassword = hashedPassword;
+
+    Object.freeze(this); // Pentru a face obiectul immutable
+  }
+
+  public static async function hashPassword(password) {
+    return bcrypt.hash(password, 10);
+  }
+
+  public function validate() {
+    if (typeof this.username !== 'string' || typeof this.password !== 'number') {
+      throw new TypeError("User.validate(): campurile nu sunt de tipurile dorite");
+    }
+
+    if (this.username === "") {
+      throw new TypeError("User.validate(): username nu poate sa fie empty");
+    }
+}
+
+function validateProdus(name, price) {
+  if (name == null || name === "" || typeof name !== "string" || price == null || typeof price !== "number" || price <= 0) {
+    throw new TypeError("Se incearca inserarea in baza de date a unui produs invalid!");
+  }
+}
+
 function insertProdus(name, price) {
+  // Validari 
+  validateProdus(name, price);
+
   cumparaturiDb.prepare(`
     INSERT INTO produse(name, price) VALUES(?, ?);
   `).run(name, price);
@@ -32,6 +64,26 @@ function createProduseTable() {
       price REAL NOT NULL
     );
     `).run();
+}
+
+function createUsersTable() {
+  cumparaturiDb.prepare(`
+    CREATE TABLE IF NOT EXISTS users (
+      user_name TEXT NOT NULL PRIMARY KEY,
+      hashed_password INTEGER NOT NULL
+    );
+  `).run();
+}
+
+function insertUser(user) {
+  if (typeof user !== "User") {
+    throw new TypeError("insertUser(): user nu este obiect al clasei User!");
+  }
+  user.validate();
+
+  cumparaturiDb.prepare(`
+    INSERT INTO users VALUES(?, ?);
+  `).run(user.username, user.hashedPassword);
 }
 
 app.get('/', (req, res) => {
